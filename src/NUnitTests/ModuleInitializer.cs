183cbecbef810ca23859e6e9980dfeed22783cc2
Simon Cropp <simon.cropp@gmail.com>
2024-07-29 20:09:46 +1000

    Update ModuleInitializer.cs

--- src/NUnitTests/ModuleInitializer.cs
+++ src/NUnitTests/ModuleInitializer.cs
@@ -1,13 +1,5 @@
 ﻿public static class ModuleInitializer
 {
-    #region enable
-
-    [ModuleInitializer]
-    public static void Init() =>
-        VerifyAssertions.Initialize();
-
-    #endregion
-
     [ModuleInitializer]
     public static void InitOther() =>
         VerifierSettings.InitializePlugins();
